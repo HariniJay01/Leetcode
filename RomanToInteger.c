74dcc295c0f98eb4e256c1a1f5cdfab0e52cb770
HariniJay01 <100073977+HariniJay01@users.noreply.github.com>
2023-02-14 19:46:25 +0530

    Update RomanToInteger.c

--- RomanToInteger.c
+++ RomanToInteger.c
@@ -21,56 +21,59 @@ Given a roman numeral, convert it to an integer.*/
 int romanToInt(char * s){
     int n=strlen(s);
     int res=0;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;){
         if(s[i]=='I'){
             if(s[i+1]=='V'){
                 res+=4;
-                s[i+1]='a';
+                i+=2;
             }
             else if(s[i+1]=='X'){
                 res+=9;
-                s[i+1]='a';
+                i+=2;
             }
             else{
                 res+=1;
+                i++;
             }
         }
 
         else if(s[i]=='X'){
-            if(s[i+1]=='L'){
-                res+=40;
-                s[i+1]='a';
-            }
-            else if(s[i+1]=='C'){
+            if(s[i+1]=='C'){
                 res+=90;
-                s[i+1]='a';
+                i+=2;
+            }
+            else if(s[i+1]=='L'){
+                res+=40;
+                i+=2;
             }
             else{
                 res+=10;
+                i++;
             }
         }
 
         else if(s[i]=='C'){
             if(s[i+1]=='D'){
                 res+=400;
-                s[i+1]='a';
+                i+=2;
             }
             else if(s[i+1]=='M'){
                 res+=900;
-                s[i+1]='a';
+                i+=2;
             }
             else{
                 res+=100;
-                s[i+1]='a';
+                i++;
             }
         }
         else if(s[i]=='V'){
             res+=5;
+            i++;
         }
-        else if(s[i]=='L'){res+=50;}
-        else if(s[i]=='D'){res+=500;}
-        else if(s[i]=='M'){res+=1000;}
-        else{continue;}
+        else if(s[i]=='L'){res+=50;i++;}
+        else if(s[i]=='D'){res+=500;i++;}
+        else if(s[i]=='M'){res+=1000;i++;}
+        else{break;}
     }
     return res;
 }
